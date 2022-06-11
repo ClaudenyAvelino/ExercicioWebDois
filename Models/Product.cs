3884a022c13377ce8527dd6c6ca7d1a55b5ca06c
claudenyAvelino <claudeny.avelino78@academico.ifs.edu.br>
2022-06-11 20:47:17 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Description { get; set; }
+        public string Fabricante { get; set; }
+    }
+}
